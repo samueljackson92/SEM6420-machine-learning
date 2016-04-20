306e0d9ec0cef9be634e6cb517c765e2743e5aed
Samuel Jackson <samueljackson@outlook.com>
2016-04-20 23:03:19 +0100

    Checkpoint work.
    Better use of pipelines
    Custom ROC scorer to collect the curve data

--- src/roc_analysis.py
+++ src/roc_analysis.py
@@ -3,79 +3,72 @@ import numpy as np
 from scipy import interp
 import matplotlib.pyplot as plt
 
-class ROCData():
+class ROCAnalysisScorer:
     def __init__(self):
-        self._j = None
-        self._area = None
-        self._fpr = None
-        self._tpr = None
+        self.rates_ = []
+        self.aucs_ = []
+
+    def __call__(self, ground_truth, predictions, **kwargs):
+        return self.auc_score(ground_truth, predictions, **kwargs)
+
+    def auc_score(self, ground_truth, predictions, **kwargs):
+        fpr, tpr, _ = metrics.roc_curve(ground_truth, predictions[:, 1])
+        area = metrics.roc_auc_score(ground_truth, predictions[:, 1], **kwargs)
+
+        self.rates_.append((fpr, tpr))
+        self.aucs_.append(area)
+        return area
+    
+    def mean_roc_metrics(self):
+        mean_tpr = 0.0
+        mean_fpr = np.linspace(0, 1, 100)
+
+        for fpr, tpr in self.rates_:
+            mean_tpr += interp(mean_fpr, fpr, tpr) 
+
+        mean_tpr = mean_tpr / len(self.rates_)
+        area =  metrics.auc(mean_fpr, mean_tpr)
+        return mean_fpr, mean_tpr, area
+
+    
+    def plot_roc_curve(self, title=None, labels=None, chance_line=False, mean_line=False):
+        for i, ((fpr, tpr), area) in enumerate(zip(self.rates_, self.aucs_)):
+            if labels is None:
+                name = "ROC %d" % (i+1)
+            elif isinstance(labels, list):
+                name = labels[i]
+            elif isinstance(labels, str):
+                name = labels
+            plt.plot(fpr, tpr, label='%s AUC = %0.2f' % (name, area))
+
+        if mean_line and len(self.rates_) > 1:
+            mean_fpr, mean_tpr, area = self.mean_roc_metrics()
+            plt.plot(mean_fpr, mean_tpr, "--", label="Mean, AUC: %.2f" % area)
+
+        if chance_line:
+            line = np.arange(0, 1.1, 0.1)
+            plt.plot(line, line, "--", label="Chance")
+
+        if title is None:
+            title = 'Receiver operating characteristic' 
         
-    @property
-    def j_stat(self):
-        return self._j
-    
-    @property
-    def auc(self):
-        return self._area
-    
-    @property
-    def fpr(self):
-        return self._fpr
-    
-    @property
-    def tpr(self):
-        return self._tpr
-    
-def sensitivity_and_specificity(cm):
-    tp, tn, fp, fn = cm[0,0], cm[1,1], cm[0,1], cm[1,0]
-    specificity = tn / float(tn + fp)
-    sensitivity = tp / float(tp + fn)
-    return sensitivity, specificity
+        plt.title(title)
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.legend(loc="lower right", prop={'size': 10})
 
-def youden_index(cm):
-    sensitivity, specificity = sensitivity_and_specificity(cm)
-    return sensitivity + specificity - 1
 
-def roc_metrics(x_data, y_data, model):
-    preds = model.predict_proba(x_data)[:, 1]
-    preds_discrete = model.predict(x_data)
-
-    fpr, tpr, _ = metrics.roc_curve(y_data, preds)
-    area = metrics.auc(fpr, tpr)
+def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
     
-    cm = metrics.confusion_matrix(y_data, preds_discrete)
-    j_stat = youden_index(cm)
-    
-    data = ROCData()
-    data.j_stat = j_stat
-    data.fpr = fpr
-    data.tpr = tpr
-    data.auc = area
-    return data
-
-def mean_roc_metrics(mets):
-    mean_tpr = 0.0
-    mean_fpr = np.linspace(0, 1, 100)
-    
-    for i, data in enumerate(mets):
-        mean_tpr += interp(mean_fpr, data.fpr, data.tpr) 
-
-    mean_tpr = mean_tpr / len(mets)
-    area =  metrics.auc(mean_fpr, mean_tpr)
-    return mean_fpr, mean_tpr, area
-
-def plot_roc(mets, title="ROC Curve"):
-    """ Plot an ROC cruve for the given y and yhat"""
-    
-    for i, data in enumerate(mets):
-        plt.plot(data.fpr, data.tpr, label="ROC Split %d, AUC: %.2f, J: %.2f" % ((i+1), data.auc, data.j_stat))
-    
-    mean_fpr, mean_tpr, area = mean_roc_metrics(mets)
-    plt.plot(mean_fpr, mean_tpr, "--", label="Mean, AUC: %.2f" % area)
-    plt.plot(np.arange(0, 1.1, 0.1), np.arange(0, 1.1, 0.1), "--", label="Chance")
+    width, height = cm.shape
+    for x in xrange(width):
+        for y in xrange(height):
+            plt.annotate(str(cm[x][y]), xy=(y, x), 
+                    horizontalalignment='center',
+                    verticalalignment='center', size=20)
 
     plt.title(title)
-    plt.ylabel("True Positive Rate")
-    plt.xlabel("False Positive Rate")
-    plt.legend(loc="lower right", prop={'size':8})
+    plt.colorbar()
     plt.show()
+    

67b36f8744ccd56c072fa7ac01fbdba7e2c63d46
Samuel Jackson <samueljackson@outlook.com>
2016-05-11 13:06:19 +0100

    Checkpoint work

--- src/pipeline.py
+++ src/pipeline.py
@@ -3,6 +3,7 @@ from sklearn import cross_validation
 from sklearn.metrics import make_scorer
 from unbalanced_dataset import SMOTE, UnderSampler, OverSampler
 
+
 def cv_pipeline(model, x_data, y_data, cv=None):
     """ Cross Validate a model pipeline
 
@@ -12,14 +13,26 @@ def cv_pipeline(model, x_data, y_data, cv=None):
         y_data: Class labels
         cv: A predefined sklearn cross validation object
     Returns:
-        A ROCAnalysisScorer object with the true/false positive rates and AUCs for all folds
+        A ROCAnalysisScorer object with the true/false positive rates and AUCs
+        for all folds
     """
     roc_data = ROCAnalysisScorer()
     roc_data_scorer = make_scorer(roc_data, greater_is_better=True, needs_proba=True, average='weighted')
     cross_validation.cross_val_score(model, x_data, y_data, cv=cv, scoring=roc_data_scorer)
     return roc_data
 
+
 def test_pipeline(model, x_data, y_data, x_test, y_test):
+    """ Test a model on a data
+
+    Args:
+        model: A sklearn Pipeline object to test
+        x_data: Feature matrix
+        y_data: Class labels
+    Returns:
+        A ROCAnalysisScorer object with the true/fals positive rates and AUCS
+        for the test
+    """
     model.fit(x_data, y_data)
     y_hat = model.predict_proba(x_test)
 
@@ -29,6 +42,16 @@ def test_pipeline(model, x_data, y_data, x_test, y_test):
 
 
 def score_pipeline(data, cv=None):
+    """ Score a pipline using either cross validation (if a cross validation
+    object is provided) or using a training/test split
+
+    Args:
+        data: A dictionary object with the model and training or testing data
+        cv: Optional cross validation object to use
+    Returns:
+        A tuple of cross validation results and testing results
+    """
+
     model = data['model']
 
     cv_results = None
@@ -47,6 +70,21 @@ def score_pipeline(data, cv=None):
 
 
 def repeated_cross_fold_validation(models, n=10, k=5):
+    """ Run cross validation on a set of models n times
+
+    All models are tested using the same cross validation splits
+    at each iteration.
+
+    Args:
+        models: List of dictionaries containing the model
+        and training or testing data.
+        n: number of iterations to repeat cross validation (default 10)
+        k: number of folds to use at each iteration (default 5)
+    Returns:
+        A list of scorer objects of type ROCAnalysisScorer, one for each model
+        passed.
+    """
+
     scorers = {}
 
     for i in range(n):
@@ -74,6 +112,23 @@ def repeated_cross_fold_validation(models, n=10, k=5):
 
 
 def monte_carlo_validation(x_data, y_data, models, splitter, n=10):
+    """ Run Monte Carlo cross validation on a set of models n times.
+
+    This will randomly split the training and test data n times
+    and evaluate the performance of each model on each split.
+
+    Args:
+        x_data: Feature matrix
+        y_data: Class labels
+        models: List of dictionaries containing the model and
+        training or testing data.
+        splitter: A test splitter object that creates random training
+        test splits.
+        n: number of iterations to perform (default 10)
+    Returns:
+        A list of scorer objects of type ROCAnalysisScorer, one for each
+        model passed.
+    """
     scorers = {}
 
     for i in range(n):
@@ -103,7 +158,12 @@ def monte_carlo_validation(x_data, y_data, models, splitter, n=10):
 
 
 class TestSplitter(object):
+    """ TestSplitter base class.
 
+    This splits a feature matrix and class labels vector
+    into a training and testing split. This can be used to
+    create more complicated splitters for resampling.
+    """
     def __init__(self, test_size=0.2):
         self._test_size = test_size
 
@@ -114,7 +174,11 @@ class TestSplitter(object):
 
 
 class SMOTESplitter(TestSplitter):
+    """ Test splitter for SMOTE datasets.
 
+    This splitter will apply smote the the training portion of the dataset
+    but will leave the testing part of the split untouched.
+    """
     def __init__(self, under_sample=1.0, smote_params={}, **kwargs):
         super(SMOTESplitter, self).__init__(**kwargs)
         self._under_sample = under_sample
@@ -126,8 +190,14 @@ class SMOTESplitter(TestSplitter):
         Xt_smote, Yt_smote = UnderSampler(ratio=self._under_sample).fit_transform(Xt_smote, Yt_smote)
         return Xt_smote, Yt_smote, Xv, Yv
 
-class OverUnderSplitter(TestSplitter):
 
+class OverUnderSplitter(TestSplitter):
+    """ Test splitter for under and/or over sampling datasets.
+
+    This splitter will apply under and/or over sampling the the training
+    portion of the dataset but will leave the testing part of the split
+    untouched.
+    """
     def __init__(self, under_sample=1.0, over_sample=1.0, **kwargs):
         super(OverUnderSplitter, self).__init__(**kwargs)
         self._under_sample = under_sample
--- src/roc_analysis.py
+++ src/roc_analysis.py
@@ -5,6 +5,14 @@ import matplotlib.pyplot as plt
 from scipy.stats import threshold
 
 class ROCAnalysisScorer:
+    """ Custom scorer to capute both the AUC score and the ROC curves.
+
+    A normal scorer for sklearn cannot capture multiple scores at once.
+    This object allows use to calculate multiple quantiies in one pass
+    of a cross validation object.
+
+    This will also capture the F scores
+    """
     def __init__(self):
         self.rates_ = []
         self.aucs_ = []
@@ -13,15 +21,32 @@ class ROCAnalysisScorer:
         self.f1scores_ = []
 
     def __call__(self, ground_truth, predictions, **kwargs):
+        """ Custom __call__ function to make the object look like a
+        function thanks to python's duck typing.
+        """
         return self.auc_score(ground_truth, predictions, **kwargs)
 
     def auc_score(self, ground_truth, predictions, **kwargs):
+        """ Calculate the AUC score for this particular trial.
+
+        This will also calculate the F scores and ROC curves
+
+        Args:
+            ground_truth: vector of class labels
+            predictions: vector of predicted class labels
+
+        Returns:
+            AUC score for this trial
+        """
+
+        # calculate f scores
         thresholded = threshold(predictions[:, 1], threshmin=0.5)
         thresholded = threshold(thresholded, threshmax=0.5, newval=1.0).astype(int)
         fhalf_score = metrics.fbeta_score(ground_truth.astype(int), thresholded, beta=0.5)
         f2_score = metrics.fbeta_score(ground_truth.astype(int), thresholded, beta=2)
         f1_score = metrics.fbeta_score(ground_truth.astype(int), thresholded, beta=1)
 
+        # calculate ROC curve and AUC
         fpr, tpr, _ = metrics.roc_curve(ground_truth, predictions[:, 1])
         area = metrics.auc(fpr, tpr)
 
@@ -33,6 +58,7 @@ class ROCAnalysisScorer:
         return area
 
     def mean_roc_metrics(self):
+        """ Compute the mean AUC and mean ROC curve """
         mean_tpr = 0.0
         mean_fpr = np.linspace(0, 1, 100)
 
@@ -43,9 +69,17 @@ class ROCAnalysisScorer:
         area =  metrics.auc(mean_fpr, mean_tpr)
         return mean_fpr, mean_tpr, area
 
-
     def plot_roc_curve(self, title=None, labels=None, show_all=True, chance_line=False, mean_line=False, mean_label="Mean"):
+        """ Plot ROC curves for all trials attached to this object
 
+        Args:
+            title: the title for the plot
+            labels: the labels to use for each line. Either list, string or None
+            show_all: show all plots or only the mean line
+            chance_line: show the chance line
+            mean_line: show the mean line
+            mean_label: label for the mean line
+        """
         if show_all:
             for i, ((fpr, tpr), area) in enumerate(zip(self.rates_, self.aucs_)):
                 if labels is None:
@@ -74,6 +108,13 @@ class ROCAnalysisScorer:
 
 
 def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues):
+    """ Plot a confusion matrix
+
+    Args:
+        cm: square matrix representing the confusion matrix
+        title: title to show on the plot
+        cmap: the colour map to use
+    """
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
 
     width, height = cm.shape
